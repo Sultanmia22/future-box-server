862ddbe161f28f502bb4aa3d3b5cd63cd4bcf54a
Sultan Mia <sultanmia5732@gmail.com>
2025-11-11 22:44:15 +0600

    added unfavorite api

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run() {
             res.send(result)
         })
 
+        //! unfavoriting artwork api 
+        app.delete('/unfavorite/:id',async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await favourtiteCollection.deleteOne(query)
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
