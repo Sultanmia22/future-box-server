5b75a51d04a380d9240872f4d01dad62c6c7f4f2
Sultan Mia <sultanmia5732@gmail.com>
2025-11-13 00:26:37 +0600

    added get all category api and get data by filter api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
-        strict: true,
+        strict: false,
         deprecationErrors: true,
     }
 });
@@ -22,118 +22,137 @@ async function run() {
     try {
 
         await client.connect();
-        
+
         const artworksDB = client.db('artifyDB');
         const artworkCollection = artworksDB.collection('artworks');
         const favourtiteCollection = artworksDB.collection('favouriteArtworks');
 
 
         //! artwork related api
-        app.get('/artworks',async(req,res) => {
-            const result = await artworkCollection.find().sort({created_at:-1}).limit(6).toArray()
+        app.get('/artworks', async (req, res) => {
+            const result = await artworkCollection.find().sort({ created_at: -1 }).limit(6).toArray()
             res.send(result)
         })
 
         //! view details api 
-        app.get('/viewDetails/:id',async(req,res) => {
+        app.get('/viewDetails/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await artworkCollection.findOne(query)
             res.send(result)
         })
 
         //! count like incress in DB 
-        app.patch('/likeCount/:id',async(req,res) => {
+        app.patch('/likeCount/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const update = {
-                $inc:{
+                $inc: {
                     like_count: 1
                 }
             }
 
-            const result = await artworkCollection.updateOne(query,update);
+            const result = await artworkCollection.updateOne(query, update);
             res.send(result);
         })
 
         //! Insert favourite artwork in favoutite db
-        app.post('/favouriteArt',async(req,res) => {
+        app.post('/favouriteArt', async (req, res) => {
             const favData = req.body;
             const result = await favourtiteCollection.insertOne(favData)
             res.send(result)
         })
 
         //! explore artwork api 
-        app.get('/explore',async(req,res) => {
+        app.get('/explore', async (req, res) => {
             const result = await artworkCollection.find().toArray();
             res.send(result);
         })
 
         //! Search artwork api 
-        app.get('/search',async(req,res) => {
+        app.get('/search', async (req, res) => {
             const searchText = req.query.search;
-            const result = await artworkCollection.find({title:{$regex:searchText,$options:'i'}}).toArray();
+            const result = await artworkCollection.find({ title: { $regex: searchText, $options: 'i' } }).toArray();
             res.send(result);
         })
 
         //! My Favourite artwork api
-        app.get('/myfavourite',async(req,res) => {
+        app.get('/myfavourite', async (req, res) => {
             const email = req.query.email;
-            const result = await favourtiteCollection.find({user_email:email}).toArray();
+            const result = await favourtiteCollection.find({ user_email: email }).toArray();
             res.send(result)
         })
 
         //! unfavoriting artwork api 
-        app.delete('/unfavorite/:id',async(req,res) => {
+        app.delete('/unfavorite/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await favourtiteCollection.deleteOne(query)
             res.send(result);
         })
 
         //! Added artwork api
-        app.post('/addartwork',async(req,res)=> {
+        app.post('/addartwork', async (req, res) => {
             const data = req.body;
             const result = await artworkCollection.insertOne(data)
             res.send(result);
         })
 
         //! my gallery get api 
-        app.get('/mygallery',async(req,res) => {
+        app.get('/mygallery', async (req, res) => {
             const query = req.query.email;
-            const result = await artworkCollection.find({email:query}).toArray();
+            const result = await artworkCollection.find({ email: query }).toArray();
             res.send(result)
         })
 
         //! My Gallery delete api 
-        app.delete('/mygalleryDetele/:id',async(req,res) => {
+        app.delete('/mygalleryDetele/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await artworkCollection.deleteOne(query)
             res.send(result)
         })
 
         //! get data for update my gallery 
-        app.get('/updateGellary/:id',async(req,res) => {
+        app.get('/updateGellary/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await artworkCollection.findOne(query)
             res.send(result)
         })
 
         //! My Gallery update api 
-        app.patch('/updateMyGallery/:id',async(req,res) => {
+        app.patch('/updateMyGallery/:id', async (req, res) => {
             const id = req.params.id
             const data = req.body;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const update = {
-                $set:{
+                $set: {
                     ...data
                 }
             }
 
-            const result = await artworkCollection.updateOne(query,update)
+            const result = await artworkCollection.updateOne(query, update)
             res.send(result)
+        })
+
+        //! get all category 
+        app.get('/categorys', async (req, res) => {
+            try {
+                const result = await artworkCollection.distinct('category')
+                res.send(result)
+            }
+            catch (error) {
+                console.error('Error:', error) // এখন error দেখতে পাবেন
+                res.status(500).send({ error: error.message })
+            }
+        })
+        
+        //! filter by category api
+        app.get('/category', async (req, res) => {
+            const category = req.query.category;
+            const result = await artworkCollection.find({ category: { $regex: category, $options: 'i' } }).toArray();
+            res.send(result);
         })
 
         await client.db("admin").command({ ping: 1 });
